e1c1af08706de54c176d0bba9be30f5e9b805d85
skoogh00 <skoogh@student.chalmers.se>
2023-12-05 07:41:09 +0100

    femman e wack

---
+++ five/five.py
@@ -0,0 +1,90 @@
+import re
+
+def first(input_path="", test=None):
+    if not test:
+        with open(input_path, "r") as file:
+            lines = file.readlines()
+    else:
+        lines = test
+    print(lines)
+
+    del lines[1]
+
+    seeds = []
+    s2s = []
+    s2f = []
+    f2w = []
+    w2l = []
+    l2t = []
+    t2h = []
+    h2l = []
+    tmp = []
+    maps = []
+    for line in lines:
+        if not seeds:
+            seeds = line
+        else:
+            tmp.append(line)
+        if line == "\n":
+            if not s2s:
+                s2s = tmp[1:len(tmp)]
+                maps.append(s2s)
+                tmp = []
+            elif not s2f:
+                s2f = tmp[1:len(tmp)]
+                maps.append(s2f)
+                tmp = []
+            elif not f2w:
+                f2w = tmp[1:len(tmp)]
+                maps.append(f2w)
+                tmp = []
+            elif not w2l:
+                w2l = tmp[1:len(tmp)]
+                maps.append(w2l)
+                tmp = []
+            elif not l2t:
+                l2t = tmp[1:len(tmp)]
+                maps.append(l2t)
+                tmp = []
+            elif not t2h:
+                t2h = tmp[1:len(tmp)]
+                maps.append(t2h)
+                tmp = []
+
+    h2l = tmp[1:len(tmp)]
+    maps.append(h2l)
+    seeds = [[seed.strip()] for seed in seeds.split(":")[1].split(" ")[1:]]
+    
+    for idx, map in enumerate(maps):
+        cmap = convert_map(map)
+        for seed in seeds:
+            t = [y for x,y in cmap if x == int(seed[idx])]
+            print("går bra!")
+            if t:
+                seed.append(t[0])
+            else:
+                seed.append(seed[idx])
+
+    res = 10000000000000000;
+    for seed in seeds:
+        res = min(res, seed[-1])
+
+    print(res)
+    
+def convert_map(map):
+    n = []
+    print("går bra")
+    for c in map[0:-1]:
+        c = c.strip().split(" ")
+        dest = range(int(c[0]), int(c[0])+int(c[2]))
+        source = range(int(c[1]), int(c[1])+int(c[2]))
+        n += list( zip(list(source), list(dest) ))
+        print("jöttebra")
+    return n
+
+
+
+if __name__ == "__main__":
+    # res = first("test.txt")
+    res = first("input.txt")
+    print(res)
